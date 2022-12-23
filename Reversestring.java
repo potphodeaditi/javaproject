793fed468490bec1dfbfb4b8b7a11be4500d8beb
potphodeaditi <121276947+potphodeaditi@users.noreply.github.com>
2022-12-23 16:07:23 +0530

    Add files via upload

---
+++ Reversestring.java
@@ -0,0 +1,29 @@
+package javapp;
+
+import java.util.Scanner;
+
+public class Reversestring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter your String");
+
+
+			StringBuilder stb=new StringBuilder();
+			stb.append(sc.nextLine());
+			System.out.println(stb.reverse());
+			String st=stb.toString();
+			System.out.println("Change into upper case  ="+st.toUpperCase());
+				 
+				   
+				
+			
+				// TODO Auto-generated method stub
+
+			}
+
+
+	}
+
+
---
+++ String1.java
@@ -0,0 +1,34 @@
+package javapp;
+
+import java.util.Scanner;
+
+public class String1 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter ypur two string");
+		
+		
+		String a1= s.nextLine();
+		String b1= s.nextLine();
+		
+		
+		if(a1.equals(b1))
+		{
+			System.out.println("both string are same");
+		}
+		else
+		{
+			System.out.println("both string are not same");
+		}
+		
+		
+		// TODO Auto-generated method stub
+
+
+   }
+		// TODO Auto-generated method stub
+
+	}
+
+
